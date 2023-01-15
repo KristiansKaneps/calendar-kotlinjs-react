19a57fb62324100f1da1d299001d490829ed72db
Kristiāns Kaņeps <kanepskristians@gmail.com>
2023-01-15 16:52:55 +0200

    fix

--- src/main/kotlin/helpers/CalendarInstance.kt
+++ src/main/kotlin/helpers/CalendarInstance.kt
@@ -52,9 +52,9 @@ class CalendarInstance(
     }
 
     fun isSameDay(date1: Date?, date2: Date?): Boolean {
-        return date1?.getDate() == date2?.getDate()
-//        return date1?.getFullYear() == date2?.getFullYear() &&
-//                date1?.getMonth() == date2?.getMonth() &&
-//                date1?.getDate() == date2?.getDate()
+//        return date1?.getDate() == date2?.getDate()
+        return date1?.getFullYear() == date2?.getFullYear() &&
+                date1?.getMonth() == date2?.getMonth() &&
+                date1?.getDate() == date2?.getDate()
     }
 }
